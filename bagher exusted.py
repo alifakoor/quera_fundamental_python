f2523baacdd3c7bd9cc7a4974ec4489d307708a9
Ali Fakoor <ali.fakoor13@gmail.com>
2021-03-17 08:47:16 +0330

    solve bagher exusted problem

--- bagher exusted.py
+++ bagher exusted.py
@@ -1,2 +1,25 @@
 x = [char for char in input()]
 num = int(''.join(x))
+n = len(x)
+index = list(range(n))
+cycle = list(range(n, 0, -1))
+result = []
+while n:
+    for i in reversed(range(n)):
+        cycle[i] -= 1
+        if cycle[i] == 0:
+            index[i:] = index[i+1:] + index[i:i+1]
+            cycle[i] = n - i
+        else:
+            j = cycle[i]
+            index[i], index[-j] = index[-j], index[i]
+            helper = int(''.join(tuple(x[i] for i in index[:n])))
+            if helper > num:
+                result.append(helper)
+            break
+    else:
+        break
+if not len(result):
+    print(0)
+else:
+    print(min(result))
---
+++ porreshte.py
@@ -0,0 +1,21 @@
+n = int(input())
+strings = ['']*100
+res = []
+for i in range(n):
+    command = [char for char in input().split()]
+    if command[0] == '1':
+        strings[int(command[1])-1] += command[3]
+    elif command[0] == '2':
+        index_str = [char for char in strings[int(command[1])-1]]
+        s = command[3]
+        result = 0
+        for j in range(len(index_str)-1):
+            helper = []
+            for k in range(len(s)):
+                if k+j < len(index_str):
+                    helper.append(index_str[(k+j) % len(index_str)])
+            if ''.join(helper) == s:
+                result += 1
+        res.append(result)
+for x in res:
+    print(x)
